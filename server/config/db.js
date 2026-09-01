568b1d10885ac9bdef0894007c27fa6a76e978d8
simplysaadali <ali091g@gmail.com>
2026-09-01 23:05:09 +0500

    Basic code with model and connection with databse done

---
+++ server/config/db.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const url = process.env.DB_URL;
+
+async function connectDB (){
+    try {
+        await mongoose.connect(url);
+        console.log("Connected to the DB successfully");
+    } catch (error) {
+        console.error("Error connecting to the Data Base: ", error);
+    }
+}
+
+export default connectDB;
+
+/*if using 
+    export default connectDB => import connectDb from "/config/db.js"
+
+but if we use
+    module.exports = {
+        connectDB,
+    }
+ => const {connectDb} = require("/config/db") */
