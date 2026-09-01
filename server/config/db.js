8c87d88ad2f65fbfc006044fc8fe66b297538023
simplysaadali <ali091g@gmail.com>
2026-09-02 01:55:47 +0500

    logical error correction, some changes and main file addition

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,13 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
 import mongoose from "mongoose";
 
-const url = process.env.DB_URL;
+// const url = process.env.DB_URL;
 
 async function connectDB (){
     try {
-        await mongoose.connect(url);
+        await mongoose.connect(process.env.DB_URL);
         console.log("Connected to the DB successfully");
     } catch (error) {
         console.error("Error connecting to the Data Base: ", error);
---
+++ server/index.js
@@ -0,0 +1,69 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+// import mongoose from "mongoose";
+import connectDB from "./config/db.js"
+import User from "./models/User.js";
+
+const PORT = process.env.PORT;
+const code = express();
+code.use(express.json());
+
+connectDB();
+
+code.get("/users", async (req, res) => {
+    try {
+        const users = await User.find().sort({ date: -1 });
+    res.status(200).json({
+        success: true,
+        data: users,
+    });
+    } catch (error) {
+        console.error("Error fetching users: ", error);
+        res.status(404).json({
+            success: false,
+            message: "Users not found",
+        });
+    }
+});
+
+code.get("/users/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200),sjon({
+            success: true,
+            data: user,
+        });
+
+        if(!user) return res.status(404).json({
+            message: "User not found",
+            success: false,
+        })
+    } catch (error) {
+        console.error("Error fetching user: ", error);
+        res.status(400).json({
+            success: false,
+            message: "Error fetching user"
+        });
+    }
+});
+
+code.post("/users", async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({
+            success: true,
+            // message: "User Created Successfully",
+            data: user,
+        });
+    } catch (error) {
+        console.error("Error creating user: ", error);
+        res.status(400).json({
+            success: false,
+            message: "Error creating user",
+        });
+    }
+});
+
+code.listen(PORT, () => console.log("API running on port " + PORT));
