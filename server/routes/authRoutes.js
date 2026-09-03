09510ac794db41b4a7b85289a6b91e80f929837f
simplysaadali <ali091g@gmail.com>
2026-09-03 21:17:48 +0500

    Intermediate level started for routes and using some dependencies for code management

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,104 @@
+import express from "express";
+import User from "./models/User.js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if(!email || !name || !password ){
+            return res.status(400).json({
+                success: false,
+                message: "All fields required!",
+            });
+        }
+
+        const findUser = await User.findOne({ email });
+
+        if(findUser){
+            return res.json({
+                message: "Email already registered!"
+            })
+        }
+
+        const user = await User.create({ name, email, password })
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully!",
+            data: user,
+        });
+        
+    } catch (error) {
+        console.error("Server Error: ", error)
+        res.status(500).json({
+            success: false,
+            message: "Server Error",
+        });
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } =  req.body;
+        const user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(404).json({
+                message: "User not found",
+                success: false,
+            });
+        }
+
+        const userData = {
+            id: user._id,
+            name: user.name,
+            email: user.email
+        }
+
+         if(user.password !== password){
+            return res.status(400).json({
+                message: "Invalid Passsword!",
+                success: false,
+            })
+        }
+
+        if(!email || !password){
+            res.status(400).json({
+                success: false,
+                message: "Email and Password both required!",
+            });
+        }
+
+            res.status(200).json({
+            success: true,
+            message: "Login Successfull!",
+            data: userData,
+        });
+
+       
+    } catch (error) {
+        console.error("Server Error: ", error);
+        res.status(500).json({
+            success: false,
+            message: "Server Error",
+        });
+    }
+});
+
+router.post("/logout", async (req, res) => {
+    try {
+        res.status(200).json({
+            success: true,
+            message: "Logout Done!",
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Error logging out!",
+            success: false,
+        });
+    }
+});
+
+export default router;
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,109 @@
+import express from "express";
+import User from "./models/User.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.find().sort({ date: -1 });
+    res.status(200).json({
+        success: true,
+        data: users,
+    });
+    } catch (error) {
+        console.error("Error fetching users: ", error);
+        res.status(404).json({
+            success: false,
+            message: "Users not found",
+        });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if(!user){
+                return res.status(404).json({
+                message: "User not found",
+                success: false,
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: user,
+        });
+    } catch (error) {
+        console.error("Error fetching user: ", error);
+        res.status(400).json({
+            success: false,
+            message: "Error fetching user"
+        });
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({
+            success: true,
+            // message: "User Created Successfully",
+            data: user,
+        });
+    } catch (error) {
+        console.error("Error creating user: ", error);
+        res.status(400).json({
+            success: false,
+            message: "Error creating user",
+        });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+        res.status(200).json(user)
+    } catch (error) {
+        console.error("Error updating user: ", error)
+        res.status(500).json({
+            success: false,
+            message: "Error updating user",
+        });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }else{
+                res.status(200).json({
+                success: true,
+                message: "User deleted successfully",
+            });
+        }
+    } catch (error) {
+        console.error("Error deleting user: ", error);
+        res.status(400).json({
+            success: false,
+            message: "Error deleting user",
+        });
+    }
+});
+
+export default router;
