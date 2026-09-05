25eb914ecaffed9e0e3aeeba808aa98bde92d656
simplysaadali <ali091g@gmail.com>
2026-09-06 01:14:29 +0500

    Made a helper function to clean the code

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,44 +1,6 @@
 // import bcrypt from "bcryptjs";
 import User from "../models/User.js";
-import jwt from "jsonwebtoken"
 import { bcrypt } from 'bcryptjs';
-
-export const cookieOption =() => ({
-    httpOnly : true, //This means JavaScript running in the browser cannot directly read the cookie.
-    secure : process.env.NODE_ENV === "production",
-
-    // this equals to
-        // let secure;
-        // if (process.env.NODE_ENV === "production") {
-        //     secure = true; only send when https (for production)
-        // } else {
-        //     secure = false; no send if http (for development)
-        // }
-
-    sameSite: lax,
-    // "Don't send my authentication cookie freely in cross-site requests, but allow it in some normal navigation situations."
-
-    maxAge: 7 * 24 * 6 * 60 * 1000,
-});
-
-//this function creates jwt
-export const signToken = () => jwt.sign (
-    {
-        id: user._id,
-        // role: user.role
-    },
-    process.env.JWT_SECRET,
-    {
-        expiresIn: "7d",
-    }
-);
-
-export const publicUser = (u) => ({
-    _id: u._id,
-    name: u.name,
-    email: u.email,
-    // role: u.role
-});
 
 export const register = async (req, res) => {
     try {
